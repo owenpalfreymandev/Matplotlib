38fef017c40f723f5fa84441e5b60c716dd58fdd
Owen Palfreyman <owenpalfreyman09@gmail.com>
2024-06-21 20:37:36 +0100

    Add colours and different marker icons

--- scatter_plots.py
+++ scatter_plots.py
@@ -4,5 +4,5 @@ import matplotlib.pyplot as plt
 x_data = np.random.random(50) * 100
 y_data = np.random.random(50) * 100
 
-plt.scatter(x_data, y_data)
+plt.scatter(x_data, y_data, c="#00f", marker="*")
 plt.show()
