4bc2013b0f1cbcbfc04c2c4aeffc8d362d08d3ea
Owen Palfreyman <owenpalfreyman09@gmail.com>
2024-06-22 16:48:36 +0100

    Explosions and colour

--- pie_chart.py
+++ pie_chart.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 
 languages = ["C++", "C#", "Python", "Java", "Ruby"]
 votes = [20, 29, 98, 12, 44]
+explodes = [0, 0.2, 0, 0, 0] # Brings out the second item in the array slightly
+color = ["blue", "purple", "yellow", "orange", "red"] # Customizes the color of each item in the pie chart
 
-plt.pie(votes, labels=languages) # The pie chart will be based on the number of 'votes', and the tags will be 'languages'
+plt.pie(votes, labels=languages, explode=explodes, colors=color, autopct="%.1f%%") # The pie chart will be based on the number of 'votes', and the tags will be 'languages'
 plt.show()
