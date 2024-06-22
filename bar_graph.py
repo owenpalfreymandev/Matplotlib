5f8f1bf9e5156b0d1d031f6dcbc4b821cbbe50fe
Owen Palfreyman <owenpalfreyman09@gmail.com>
2024-06-22 16:31:57 +0100

    Renamed Variables

--- bar_graph.py
+++ bar_graph.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 bands = ["C++", "C#", "Python", "Java", "Ruby"]
-monthly_listeners = [20, 29, 163, 12, 44]
+votes = [20, 29, 163, 12, 44]
 
 plt.bar(bands, monthly_listeners, align="edge", width=0.5, edgecolor="orange", lw=6)
 plt.show()
