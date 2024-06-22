2bff19620b99f7f3bc2eb161b0035c610bf2d211
Owen Palfreyman <owenpalfreyman09@gmail.com>
2024-06-22 16:49:46 +0100

    Added explosions and colour (british spelling because I don't like the world 'color' but I used 'color' in the code because I felt like I had to :(

--- pie_chart.py
+++ pie_chart.py
@@ -7,4 +7,8 @@ explodes = [0, 0.2, 0, 0, 0] # Brings out the second item in the array slightly
 color = ["blue", "purple", "yellow", "orange", "red"] # Customizes the color of each item in the pie chart
 
 plt.pie(votes, labels=languages, explode=explodes, colors=color, autopct="%.1f%%") # The pie chart will be based on the number of 'votes', and the tags will be 'languages'
+
+# We can use pctdistance to change the distance of the tag and the number from the middle, but it makes it look ugly :(
+# We can also use startangle to change the starting circle
+
 plt.show()
