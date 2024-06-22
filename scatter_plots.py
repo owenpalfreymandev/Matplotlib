943bda6f80e7cf0430752fe2c89a86a2aeaeaa66
Owen Palfreyman <owenpalfreyman09@gmail.com>
2024-06-22 08:07:18 +0100

    Added transparency so user large amounts of data is visible, even if they overlap

--- scatter_plots.py
+++ scatter_plots.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x_data = np.random.random(50) * 100
-y_data = np.random.random(50) * 100
+x_data = np.random.random(50) * 1000
+y_data = np.random.random(50) * 1000
 
-plt.scatter(x_data, y_data, c="#00f", s=150, marker="*")
+plt.scatter(x_data, y_data, c="#00f", s=150, marker="*", alpha=0.3)
 plt.show()
